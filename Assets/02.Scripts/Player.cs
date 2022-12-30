900515a4f5f16831828083e03113429fd95a768f
ParkJin0814 <isklkl97@gmail.com>
2022-12-31 07:55:30 +0900

    2022.12.31 07:54
    1. 블랜딩트리를이용한 이동구현
    2. 무기집어넣는동안 이동못하게구현
    3. 달리기구현

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float PlayerSpeed;
     Animator myAnim;
+    float Run;
+    float x;
+    float y;
+    bool runing = false;
     void Start()
     {
         myAnim= GetComponent<Animator>();
@@ -12,9 +18,38 @@ public class Player : MonoBehaviour
         
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.X))
-        {
+        Movement();
 
-        }         
+    }
+    void Movement()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        myAnim.SetFloat("x", x);
+        myAnim.SetFloat("y", y);
+        if (Input.GetKeyDown(KeyCode.X)&& !myAnim.GetBool("KnifeToggle"))
+        {
+            myAnim.SetTrigger("Knife");
+            myAnim.SetBool("IsKnife", !myAnim.GetBool("IsKnife"));
+        }
+        if (Input.GetKey(KeyCode.LeftShift)&&y>0.0f)
+        {
+            Run = Mathf.Lerp(0, 1, Run + 0.02f);
+            runing=true;
+        }
+        else
+        {
+            Run = Mathf.Lerp(0, 1, Run - 0.02f);
+            runing=false;
+        }
+        myAnim.SetFloat("Run", Run);
+        Vector3 pos = transform.position;
+        pos.x += x;
+        pos.z += y;
+        float delta = runing? PlayerSpeed *1.5f* Time.deltaTime : PlayerSpeed *  Time.deltaTime;
+        Vector3 dir = pos - transform.position;
+        dir.Normalize();
+        if(!myAnim.GetBool("KnifeToggle"))transform.Translate(dir * delta, Space.World);
+
     }
 }
